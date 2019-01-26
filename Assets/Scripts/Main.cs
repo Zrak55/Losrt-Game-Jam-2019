22615b53511a7d1e1adfbc768fb346c281fad79c
MagicDogboi <45516301+MagicDogboi@users.noreply.github.com>
2019-01-26 11:48:37 -0600

    Add files via upload

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    public void startgame()
+    {
+        SceneManager.LoadScene("");   // At end
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+
+}
+
---
+++ Assets/Scripts/wackyvolumescript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class wackyvolumescript : MonoBehaviour
+{
+    public Slider sldrVolume;
+    public void Update()
+    {
+        AudioListener.volume = sldrVolume.value;
+    }
+
+}
