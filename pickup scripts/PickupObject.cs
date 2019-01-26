873b867e3c73a933e13214c59a443bd1a93a809d
NOsborn2 <47048397+NOsborn2@users.noreply.github.com>
2019-01-26 05:24:09 -0600

    Add files via upload

---
+++ pickup scripts/PickupObject.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+	GameObject mainCamera;
+	bool carrying;
+	GameObject carriedObject;
+	public float distance;
+	public float smooth;
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(carrying) {
+			carry(carriedObject);
+			checkDrop();
+			//rotateObject();
+		} else {
+			pickup();
+		}
+	}
+	
+	void rotateObject() {
+		carriedObject.transform.Rotate(5,10,15);
+	}
+	
+	void carry(GameObject o) {
+		o.transform.position = Vector3.Lerp (o.transform.position, mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+	}
+	
+	void pickup() {
+		if(Input.GetMouseButtonDown(1)) {
+			int x = Screen.width / 2;
+			int y = Screen.height / 2;
+			
+			Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+            RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				Pickupable p = hit.collider.GetComponent<Pickupable>();
+				if(p != null) {
+					carrying = true;
+					carriedObject = p.gameObject;
+					p.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                }
+			}
+		}
+	}
+	
+	void checkDrop() {
+		if(Input.GetMouseButtonDown(1)) {
+			dropObject();
+		}
+	}
+	
+	void dropObject() {
+		carrying = false;
+		carriedObject.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        carriedObject = null;
+	}
+}
+
