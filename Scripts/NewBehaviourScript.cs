e1b8573f4706a78a8892b86c9dc6c7a32afa6633
NOsborn2 <47048397+NOsborn2@users.noreply.github.com>
2019-01-26 02:12:27 -0600

    Add files via upload

---
+++ Scripts/NewBehaviourScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private bool echo;
+    public GameObject prefabLight;
+    GameObject prefabLightClone;
+    private int cooldown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        echo = false;
+        cooldown = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("q"))
+        {
+            echo = true;
+        }
+        else if(echo == true && !Input.GetKeyDown("q"))
+        {
+            echo = false;
+        }
+
+        if(echo == true && cooldown == 0)
+        {
+            prefabLightClone = Instantiate(prefabLight, transform.position, Quaternion.identity) as GameObject;
+            cooldown = 100;
+        }
+        if(cooldown != 0)
+        {
+            cooldown--;
+        }
+
+    }
+}
---
+++ Scripts/lightBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightBehavior : MonoBehaviour
+{
+    private int lifetime;
+    private static int movespeed = 3;
+    private Vector3 userDirection = Vector3.right;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifetime = 60;
+        userDirection = Camera.main.transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Countdown to object deletion
+        lifetime = lifetime - 1;
+
+        //if the Object is at the end of its life, delete it. 
+        if(lifetime == 0)
+        {
+            Destroy(gameObject);
+        }
+
+        //Move the object forwards
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+    }
+}
