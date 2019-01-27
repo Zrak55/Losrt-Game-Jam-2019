e253f05715a97616c13ca678218bc4c62e54ed02
MagicDogboi <45516301+MagicDogboi@users.noreply.github.com>
2019-01-26 22:57:21 -0600

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    public void startgame()
+    {
+        SceneManager.LoadScene("House_5");   
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+    public void startwacgame()
+    {
+        Wack.gamestatewacky = true;
+        SceneManager.LoadScene("House_5");   
+    }
+
+
+}
+
